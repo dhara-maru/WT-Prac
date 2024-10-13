ce5cc177ecf4d19dede53f8770c010fa02a4581e
dhara-maru <dharamaru406@gmail.com>
2024-10-13 10:13:33 +0530

    L9 C7 : Find word index

---
+++ WTLab9/findwordindex.js
@@ -0,0 +1,7 @@
+let sentence = "Hello this is a random sentence";
+        let words = sentence.split(" ");
+        console.log(words)
+        let search = "random";
+        let result = words.indexOf(search);
+        console.log("Found the word at : "+result);
+        
