51ac9021f1d3f2ac2aea6b29682c343d3e61eec4
dhara-maru <dharamaru406@gmail.com>
2024-11-05 14:23:39 +0530

    Math js

---
+++ Home_wt_prac/math.js
@@ -0,0 +1,19 @@
+console.log(Math.sqrt(25));     //5
+console.log(Math.ceil(10.3));       //11
+console.log(Math.floor(10.3));      //10
+console.log(Math.abs(-10));         //10
+console.log(Math.pow(2,3));         //8
+console.log(Math.sin(10));          //-0.5440211108893698
+console.log(Math.cos(10));      //-0.8390715290764524
+console.log(Math.PI);       //3.141592653589793
+console.log(Math.max(12,20,30));            //30
+console.log(Math.min(-20,-30, -40));        //-40
+console.log(Math.round(10.3));      //10
+console.log(Math.random());     //0.2786267741535011
+console.log(Math.random()*10);      //9.54710775683117
+
+
+
+
+
+
