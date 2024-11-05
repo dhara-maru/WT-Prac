eb35248bba5c0c3b02613f33e797302bdefe0a33
dhara-maru <dharamaru406@gmail.com>
2024-11-05 15:05:13 +0530

    array in js 2

--- Home_wt_prac/arrayinjs.js
+++ Home_wt_prac/arrayinjs.js
@@ -10,6 +10,15 @@ console.log(arr.indexOf(50));       //-1 bcpz 50 doesn't exist
 console.log(arr.slice(1,3));        //[1,2]
 console.log(arr.splice(1,3));   //[1,2,3]   and now og array is [0,4,5]   
 
+const gujarat = ['morbi', 'rajkot', 'surat']
+const mh = ['mumbai', 'pune', 'nashik']
+const gj_mh = [...gujarat, ...mh]       //[ 'morbi', 'rajkot', 'surat', 'mumbai', 'pune', 'nashik' ]
+const concated_gj_mh = gujarat.concat(mh)   //same as spread operator
+console.log(gj_mh);
+console.log(concated_gj_mh);
+
+const myname = "Dhara";
+console.log(Array.from(myname))     //[ 'D', 'h', 'a', 'r', 'a' ]
 
  console.log(arr);
 
