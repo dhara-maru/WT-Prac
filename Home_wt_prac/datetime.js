b2f16eeae2908f5296c7d26da3f055d02ed8e264
dhara-maru <dharamaru406@gmail.com>
2024-11-05 14:18:49 +0530

    date time

---
+++ Home_wt_prac/datetime.js
@@ -0,0 +1,18 @@
+const obj = new Date();
+
+console.log(obj);       //2024-11-05T08:45:59.980Z
+
+console.log(obj.getFullYear());     //2024
+console.log(obj.getMonth()+1);      //11
+console.log(obj.getDate());         //5
+console.log(obj.getHours());        //14
+console.log(obj.getMinutes());      //18
+console.log(obj.getSeconds());      //9
+console.log(obj.getMilliseconds());     //778
+
+
+
+
+
+
+
